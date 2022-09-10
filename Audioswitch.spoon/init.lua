679f177bd737d5d284e5aac7e4a37dce04493d92
Quin <quin.marilyn05@gmail.com>
2022-09-09 23:51:36 -0600

    V1.0

---
+++ Audioswitch.spoon/init.lua
@@ -0,0 +1,115 @@
+local inspect = hs.inspect
+
+local Audioswitch = {}
+
+Audioswitch.name = "Audioswitch"
+Audioswitch.version = "1.0"
+Audioswitch.author = "Quin Marilyn <quin.marilyn05@gmail.com>"
+Audioswitch.license = "MIT"
+Audioswitch.homepage = "https://www.github.com/TheQuinbox/Audioswitch"
+
+local speakScript = [[
+tell application "voiceover" to output "MESSAGE"
+]]
+
+local function speak(text)
+	local script = speakScript:gsub("MESSAGE", function ()
+		return text
+	end)
+	success, _, output = hs.osascript.applescript(script)
+	if not success then
+		print(inspect(output))
+	end
+end
+
+local function prevOutput()
+	local currDevice = hs.audiodevice.defaultOutputDevice()
+	local outputDevices = hs.audiodevice.allOutputDevices()
+	local index = 0
+	for i, device in pairs(outputDevices) do
+		if device == currDevice then
+			index = i
+			break
+		end
+	end
+	if index <= 1 then
+		return
+	end
+	outputDevices[index - 1]:setDefaultOutputDevice()
+	speak(outputDevices[index - 1]:name())
+end
+
+local function nextOutput()
+	local currDevice = hs.audiodevice.defaultOutputDevice()
+	local outputDevices = hs.audiodevice.allOutputDevices()
+	local index = 0
+	for i, device in pairs(outputDevices) do
+		if device == currDevice then
+			index = i
+			break
+		end
+	end
+	if index >= #outputDevices then
+		return
+	end
+	outputDevices[index + 1]:setDefaultOutputDevice()
+	speak(outputDevices[index + 1]:name())
+end
+
+local function prevInput()
+	local currDevice = hs.audiodevice.defaultInputDevice()
+	local inputDevices = hs.audiodevice.allInputDevices()
+	local index = 0
+	for i, device in pairs(inputDevices) do
+		if device == currDevice then
+			index = i
+			break
+		end
+	end
+	if index <= 1 then
+		return
+	end
+	inputDevices[index - 1]:setDefaultInputDevice()
+	speak(inputDevices[index - 1]:name())
+end
+
+local function nextInput()
+	local currDevice = hs.audiodevice.defaultInputDevice()
+	local inputDevices = hs.audiodevice.allInputDevices()
+	local index = 0
+	for i, device in pairs(inputDevices) do
+		if device == currDevice then
+			index = i
+			break
+		end
+	end
+	if index >= #inputDevices then
+		return
+	end
+	inputDevices[index + 1]:setDefaultInputDevice()
+	speak(inputDevices[index + 1]:name())
+end
+
+local prevOutputHotkey = hs.hotkey.new("ctrl-shift", "[", prevOutput)
+local nextOutputHotkey = hs.hotkey.new("ctrl-shift", "]", nextOutput)
+local prevInputHotkey = hs.hotkey.new("ctrl-cmd-shift", "[", prevInput)
+local nextInputHotkey = hs.hotkey.new("ctrl-cmd-shift", "]", nextInput)
+
+function Audioswitch.init()
+end
+
+function Audioswitch.start()
+	prevOutputHotkey:enable()
+	nextOutputHotkey:enable()
+	prevInputHotkey:enable()
+	nextInputHotkey:enable()
+end
+
+function Audioswitch.stop()
+	prevOutputHotkey:disable()
+	nextOutputHotkey:disable()
+	prevInputHotkey:disable()
+	nextInputHotkey:disable()
+end
+
+return Audioswitch
---
+++ README.md
@@ -0,0 +1,41 @@
+# Audioswitch
+
+Audioswitch is a [Hammerspoon[(https://www.hammerspoon.org) that allows you to change input/output devices on the fly with simple global hotkeys.
+
+## Installation
+
+First, download and install Hammerspoon. You can do so either [from their Github](https://github.com/Hammerspoon/hammerspoon/releases/latest), or if you have it installed, through homebrew simply by running:
+
+```bash
+$ brew install Hammerspoon
+```
+
+in the terminal. Once you have it installed, run it, and follow the prompts to grant accessibility permissions (I also choose to hide the app from the dock here so it stays out of your command-tab switcher)
+
+You also need to allow external apps to control VoiceOver through AppleScript. To do so, open VoiceOver utility with VO + f8, navigate to the "General" section and check the "Allow VoiceOver to be controlled with AppleScript" checkbox.
+
+Once Hammerspoon is installed and configured, navigate into the folder where you cloned this repository with Finder or another file manager, and open "Audioswitch.spoon" which should cause Hammerspoon to install it into the right place. Finally, from the Hammerspoon menu extra select the open configuration option which should open your default text editor with your init.lua file. To make Audioswitch work and do its thing, simply add the following 2 lines:
+
+```lua
+hs.loadSpoon("Audioswitch")
+spoon.Audioswitch:start()
+```
+
+Save the file, return to the Hammerspoon menu extra but this time click the reload configuration option for your new changes to take effect. Mac OS will warn you that Hammerspoon is trying to control Voice Over. Grant it permission and the spoon should start working.
+
+## Features
+
+| Command | Description |
+| --- | --- |
+| Control+Shift+[ | Cycle back an output device |
+| Control+Shift+] | Cycle forward an output device |
+| Control+Command+Shift+[ | Cycle back an input device |
+| Control+Command+Shift+] | Cycle forward an input device |
+
+## Contributing
+
+If there's a feature you'd like to see added, feel free to open a pull request/issue. I'd prefer issues get opened before forging ahead with huge pull requests, unless the feature is already on the todo list.
+
+## Credits
+
+Instillation instructions adapted from [Indent Beeper](https://github.com/pitermach/IndentBeeper), and [Speech History](http://github.com/mikolysz/speech-history).
