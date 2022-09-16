bfe67f7ddb2348e697bab0aa46d9b219db4a6853
Quin <quin.marilyn05@gmail.com>
2022-09-16 12:43:40 -0600

    Add stylua config, and lint using stylua.

--- Audioswitch.spoon/init.lua
+++ Audioswitch.spoon/init.lua
@@ -13,93 +13,93 @@ tell application "voiceover" to output "MESSAGE"
 ]]
 
 local function speak(text)
-	local script = speakScript:gsub("MESSAGE", function ()
-		return text
-	end)
-	success, _, output = hs.osascript.applescript(script)
-	if not success then
-		print(inspect(output))
-	end
+    local script = speakScript:gsub("MESSAGE", function()
+        return text
+    end)
+    success, _, output = hs.osascript.applescript(script)
+    if not success then
+        print(inspect(output))
+    end
 end
 
 local function prevOutput()
-	local currDevice = hs.audiodevice.defaultOutputDevice()
-	local outputDevices = hs.audiodevice.allOutputDevices()
-	local index = 0
-	for i, device in pairs(outputDevices) do
-		if device == currDevice then
-			index = i
-			break
-		end
-	end
-	if index <= 1 then
-		return
-	end
-	outputDevices[index - 1]:setDefaultOutputDevice()
-	speak(outputDevices[index - 1]:name())
+    local currDevice = hs.audiodevice.defaultOutputDevice()
+    local outputDevices = hs.audiodevice.allOutputDevices()
+    local index = 0
+    for i, device in pairs(outputDevices) do
+        if device == currDevice then
+            index = i
+            break
+        end
+    end
+    if index <= 1 then
+        return
+    end
+    outputDevices[index - 1]:setDefaultOutputDevice()
+    speak(outputDevices[index - 1]:name())
 end
 
 local function nextOutput()
-	local currDevice = hs.audiodevice.defaultOutputDevice()
-	local outputDevices = hs.audiodevice.allOutputDevices()
-	local index = 0
-	for i, device in pairs(outputDevices) do
-		if device == currDevice then
-			index = i
-			break
-		end
-	end
-	if index >= #outputDevices then
-		return
-	end
-	outputDevices[index + 1]:setDefaultOutputDevice()
-	speak(outputDevices[index + 1]:name())
+    local currDevice = hs.audiodevice.defaultOutputDevice()
+    local outputDevices = hs.audiodevice.allOutputDevices()
+    local index = 0
+    for i, device in pairs(outputDevices) do
+        if device == currDevice then
+            index = i
+            break
+        end
+    end
+    if index >= #outputDevices then
+        return
+    end
+    outputDevices[index + 1]:setDefaultOutputDevice()
+    speak(outputDevices[index + 1]:name())
 end
 
 local function prevInput()
-	local currDevice = hs.audiodevice.defaultInputDevice()
-	local inputDevices = hs.audiodevice.allInputDevices()
-	local index = 0
-	for i, device in pairs(inputDevices) do
-		if device == currDevice then
-			index = i
-			break
-		end
-	end
-	if index <= 1 then
-		return
-	end
-	inputDevices[index - 1]:setDefaultInputDevice()
-	speak(inputDevices[index - 1]:name())
+    local currDevice = hs.audiodevice.defaultInputDevice()
+    local inputDevices = hs.audiodevice.allInputDevices()
+    local index = 0
+    for i, device in pairs(inputDevices) do
+        if device == currDevice then
+            index = i
+            break
+        end
+    end
+    if index <= 1 then
+        return
+    end
+    inputDevices[index - 1]:setDefaultInputDevice()
+    speak(inputDevices[index - 1]:name())
 end
 
 local function nextInput()
-	local currDevice = hs.audiodevice.defaultInputDevice()
-	local inputDevices = hs.audiodevice.allInputDevices()
-	local index = 0
-	for i, device in pairs(inputDevices) do
-		if device == currDevice then
-			index = i
-			break
-		end
-	end
-	if index >= #inputDevices then
-		return
-	end
-	inputDevices[index + 1]:setDefaultInputDevice()
-	speak(inputDevices[index + 1]:name())
+    local currDevice = hs.audiodevice.defaultInputDevice()
+    local inputDevices = hs.audiodevice.allInputDevices()
+    local index = 0
+    for i, device in pairs(inputDevices) do
+        if device == currDevice then
+            index = i
+            break
+        end
+    end
+    if index >= #inputDevices then
+        return
+    end
+    inputDevices[index + 1]:setDefaultInputDevice()
+    speak(inputDevices[index + 1]:name())
 end
 
 local function muteInputDevice()
-	local device = hs.audiodevice.defaultInputDevice()
-	local muted = device:muted()
-	if muted then
-		device:setInputMuted(false)
-		speak("Unmuted")
-	else
-		device:setInputMuted(true)
-		speak("Muted")
-	end
+    local device = hs.audiodevice.defaultInputDevice()
+    local muted = device:muted()
+    if muted then
+        device:setInputMuted(false)
+        speak("Unmuted")
+    else
+        device:setInputMuted(true)
+        speak("Muted")
+    end
 end
 
 local prevOutputHotkey = hs.hotkey.new("ctrl-shift", "[", prevOutput)
@@ -108,23 +108,22 @@ local prevInputHotkey = hs.hotkey.new("ctrl-cmd-shift", "[", prevInput)
 local nextInputHotkey = hs.hotkey.new("ctrl-cmd-shift", "]", nextInput)
 local muteInputHotkey = hs.hotkey.new("ctrl-shift", "m", muteInputDevice)
 
-function Audioswitch.init()
-end
+function Audioswitch.init() end
 
 function Audioswitch.start()
-	prevOutputHotkey:enable()
-	nextOutputHotkey:enable()
-	prevInputHotkey:enable()
-	nextInputHotkey:enable()
-	muteInputHotkey:enable()
+    prevOutputHotkey:enable()
+    nextOutputHotkey:enable()
+    prevInputHotkey:enable()
+    nextInputHotkey:enable()
+    muteInputHotkey:enable()
 end
 
 function Audioswitch.stop()
-	prevOutputHotkey:disable()
-	nextOutputHotkey:disable()
-	prevInputHotkey:disable()
-	nextInputHotkey:disable()
-	muteInputHotkey:disable()
+    prevOutputHotkey:disable()
+    nextOutputHotkey:disable()
+    prevInputHotkey:disable()
+    nextInputHotkey:disable()
+    muteInputHotkey:disable()
 end
 
 return Audioswitch
